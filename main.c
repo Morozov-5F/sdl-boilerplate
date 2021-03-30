65aba1928e64e6507ff8a44df3f54a282601a560
Morozov-5F <morozov.evgeniy.95@gmail.com>
2021-03-30 23:55:48 +0300

    Initial commit: add SDL sample

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#include <SDL.h>
+
+int main()
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow("SDL Boilerplate", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, 0);
+
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+
+    while (1)
+    {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_QUIT:
+                    exit(0);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        SDL_RenderPresent(renderer);
+    }
+
+    return 0;
+}
