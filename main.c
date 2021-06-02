2b4542e545cabfb59f78bf4e3755e1cfe45443bc
Morozov-5F <morozov.evgeniy.95@gmail.com>
2021-06-02 17:58:48 +0300

    ADd ability to target Web Assembly.

--- main.c
+++ main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <stdbool.h>
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#endif
 #include <SDL.h>
+
+static SDL_Renderer *gs_renderer = NULL;
+static bool gs_running = false;
+
+void main_loop(void)
+{
+    SDL_Rect rect = { 100, 100, 100, 100};
+
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+            case SDL_QUIT:
+                gs_running = false;
+                return;
+
+            default:
+                break;
+        }
+    }
+
+    SDL_SetRenderDrawColor(gs_renderer, 0, 0, 0, 255);
+    SDL_RenderClear(gs_renderer);
+
+    SDL_SetRenderDrawColor(gs_renderer, 255, 0, 0, 255);
+    SDL_RenderFillRect(gs_renderer, &rect);
+
+    SDL_RenderPresent(gs_renderer);
+}
 
 int main()
 {
@@ -10,33 +44,21 @@ int main()
 
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    gs_renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
 
-    while (1)
+    gs_running = true;
+#ifndef __EMSCRIPTEN__
+    while (gs_running)
     {
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-
-        SDL_Rect rect = { 100, 100, 100, 100};
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_RenderFillRect(renderer, &rect);
-
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            switch (event.type)
-            {
-                case SDL_QUIT:
-                    exit(0);
-                    break;
-
-                default:
-                    break;
-            }
-        }
-
-        SDL_RenderPresent(renderer);
+        main_loop();
     }
+#else
+    emscripten_set_main_loop(main_loop, -1, 1);
+#endif
+
+    SDL_DestroyRenderer(gs_renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
